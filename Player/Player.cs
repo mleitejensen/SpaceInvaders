aba34ed531d5c1a3dd0bd202e15fe1e855e9b5cc
mleitejensen <mleitejensen@gmail.com>
2025-02-19 16:46:56 +0100

    added player movement

---
+++ Player/Player.cs
@@ -0,0 +1,26 @@
+using Godot;
+using System;
+
+public partial class Player : CharacterBody2D
+{
+	public const float Speed = 300.0f;
+	public const float JumpVelocity = -400.0f;
+
+	public override void _PhysicsProcess(double delta)
+	{
+		Vector2 velocity = Velocity;
+
+		float direction = Input.GetAxis("ui_left", "ui_right");
+		if (direction != 0)
+		{
+			velocity.X = direction * Speed;
+		}
+		else
+		{
+			velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
+		}
+
+		Velocity = velocity;
+		MoveAndSlide();
+	}
+}
