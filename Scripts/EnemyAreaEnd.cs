1e9ad20c478d273786197e47507e950e3760135a
Martin <mleitejensen@gmail.com>
2025-08-26 23:27:41 +0200

    refactor: added debounce to EnemyChangeDirection signal emit

--- Scripts/EnemyAreaEnd.cs
+++ Scripts/EnemyAreaEnd.cs
@@ -3,18 +3,27 @@ using System;
 
 public partial class EnemyAreaEnd : Area2D
 {
+	private float debounceTime = 0.5f;
+	private Timer debounceTimer = new();
+
+
 	public override void _Ready()
 	{
-		this.BodyEntered += OnBodyEntered;
+		debounceTimer.WaitTime = debounceTime;
+		debounceTimer.OneShot = true;
+		AddChild(debounceTimer);
+
+		BodyEntered += OnBodyEntered;
 	}
 
 	private void OnBodyEntered(Node2D body)
 	{
-		GD.Print(body.GetType());
-		if (body is Enemy)
+		if (body is Enemy && debounceTimer.IsStopped())
 		{
+			debounceTimer.Start();
 			SignalBus.Instance.EmitSignal(SignalBus.SignalName.EnemyChangeDirection);
 		}
 	}
+
 
 }
